4ad8ca7a9101f315b5d21f5d1e74fcf64cb60069
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-14 14:01:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   reversed_array = []
-  array.each {|word| array << word.reverse}
-  
+  array.each {|word| reversed_array << word.reverse}
+
 end
